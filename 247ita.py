3af073d2456bd7655103ba48dab66b1a5506f820
ciccioxm3 <cicciariello@proton.me>
2025-04-28 17:45:11 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -354,9 +354,9 @@ def generate_m3u8_247(matches):
 
             if stream_url_dynamic:
                 file.write(f"#EXTINF:-1 tvg-id=\"{tvg_id}\" tvg-name=\"{channel_name}\" tvg-logo=\"{tvicon_path}\" group-title=\"{category}\", {channel_name} (D)\n")
-                file.write(f'#EXTVLCOPT:http-referrer=https://webxzplay.cfd/\n')
+                file.write(f'#EXTVLCOPT:http-referrer=https://xextreamplay.xyz/\n')
                 file.write('#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36\n')
-                file.write('#EXTVLCOPT:http-origin=https://webxzplay.cfd\n')
+                file.write('#EXTVLCOPT:http-origin=https://xextreamplay.xyz\n')
                 file.write(f"{stream_url_dynamic}\n\n")
                 processed_247_channels += 1
             else:
